dd10f1d6b6be17dcbfba0bad6d78a653fd847863
Alexander Schroeter <as.alexanderschroeter@googlemail.com>
2016-06-11 13:19:38 +0200

    first commit

---
+++ js/services/suggestions.js
@@ -0,0 +1,27 @@
+app.factory('suggestions', [function(){
+var demoSuggestions = {
+posts: [
+{
+title: 'Nimm 5 Kilo ab',
+upvotes: 13,
+comments: [],
+},
+{
+title: 'Meditiere jeden Tag 10 min',
+upvotes: 6,
+comments: [],
+},
+{
+title: 'Baue ein eigenes Unternehmen auf',
+upvotes: 7,
+comments: [],
+},
+{
+title: 'Nutze dein Leben',
+upvotes: 14,
+comments: [],
+},
+]
+};
+return demoSuggestions;
+}]);
